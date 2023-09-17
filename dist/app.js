b546197dbc7787bcdfeaf006b212b23154d622f5
Patricia Atanasoff <system.informatica365@gmail.com>
2023-09-17 01:46:40 -0300

    scroll-up, change header color

--- dist/app.js
+++ dist/app.js
@@ -16,3 +16,33 @@ navLink.forEach(link =>
         navMenu.classList.add('hidden')
     })
 )
+
+// Scroll up
+
+const scrollUp = () =>{
+    const scrollBtn = document.getElementById('top')
+
+    if(this.scrollY >= 250){
+        scrollBtn.classList.remove("-bottom-1/2")
+        scrollBtn.classList.add("bottom-4")
+    }else{
+        scrollBtn.classList.add("-bottom-1/2")
+        scrollBtn.classList.remove("bottom-4")
+    }
+}
+
+window.addEventListener('scroll', scrollUp)
+
+// Change color header
+
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+
+    if(this.scrollY >= 50){
+        header.classList.add("border-b-2", "border-amber-400")
+    }else{
+        header.classList.remove("border-b-2", "border-amber-400")
+    }
+}
+
+window.addEventListener('scroll', scrollHeader)
